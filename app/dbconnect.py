8f76a8347da0a2d76468f06b3c711ea5e66be408
Shakila shaik <skshakilask67@gmail.com>
2025-08-13 15:34:43 +0530

    syntax issue

--- app/dbconnect.py
+++ app/dbconnect.py
@@ -8,7 +8,7 @@ engine = create_async_engine(
     connect_args={"statement_cache_size": 0},  # Disable prepared stmt cache
     pool_size=5,      # Prevent too many connections
     max_overflow=0 ,
-    pool_class=None # No overflow beyond pool_size
+    poolclass=None # No overflow beyond pool_size
 )
 
 # Session factory
